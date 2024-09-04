99c4255b2fc1709da9d40677d27cc48fedd8ab7b
tony-ngok <tonyn1999@outlook.com>
2024-09-04 09:54:43 -0500

    抓取所有商品子分类

---
+++ emscraper/emscraper/spiders/po_cats.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.http import HtmlResponse
+
+
+# scrapy crawl myspider00 -O mydata00.json
+class POCategories(scrapy.Spider):
+    name = "po_cats"
+    allowed_domains = ['www.pharmacyonline.com.au']
+    start_urls = ['https://www.pharmacyonline.com.au/']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "es-ES,es;q=0.8,en-GB;q=0.5,en;q=0.3",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0"
+        }
+    
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], headers=self.headers, callback=self.parse)
+
+    def parse(self, response: HtmlResponse):
+        l_items = response.css('li[data-title="Shop by category"] li.item > a::attr(href)').getall()
+        parx = set(response.css('li[data-title="Shop by category"] li.item.-parent > a::attr(href)').getall())
+        l_cats = [l for l in l_items if l not in parx]
+
+        for l in l_cats:
+            yield {
+                "cat_link": l
+            }
