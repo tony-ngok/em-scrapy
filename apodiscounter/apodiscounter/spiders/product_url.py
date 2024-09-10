9307957f8abf3d6c3e93db55c08135747473edc9
tony-ngok <tonyn1999@outlook.com>
2024-09-10 17:03:32 -0500

    produkte

--- apodiscounter/apodiscounter/spiders/product_url.py
+++ apodiscounter/apodiscounter/spiders/product_url.py
@@ -64,6 +64,8 @@ class ProductUrlSpider(scrapy.Spider):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0"
         }
 
+        self.produkte = set()
+
     def start_requests(self):
         for url in ProductUrlSpider.start_urls:
             if "products" not in url:
