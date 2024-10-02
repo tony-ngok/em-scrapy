e4021e4e94cfcbedde5f967afdfefd4b2b45e758
tony-ngok <tonyn1999@outlook.com>
2024-10-02 16:38:27 -0500

    default

--- amazontr/amazontr/settings.py
+++ amazontr/amazontr/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = "amazontr.spiders"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 12
+#CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
