8cf9f69586093300a8b360b5806eb080ed10e54f
tony-ngok <tonyn1999@outlook.com>
2024-10-17 10:50:20 -0500

    试运行

--- naver_handmade/handmade_auto.bat
+++ naver_handmade/handmade_auto.bat
@@ -4,28 +4,28 @@
 cd /d "%~dp0"
 git pull
 
-@REM 从大分类中抓取品牌子分类
-:cat_brand
-echo Start program cat_brand.py...
-python cat_brand.py
-echo Exit code: %ERRORLEVEL%
-if %ERRORLEVEL% neq 0 (
-    echo Program cat_brand.py ends with errors. Rerun...
-    goto cat_brand_review
-)
-echo Program cat_brand.py success
-goto end
+@REM @REM 从大分类中抓取品牌子分类
+@REM :cat_brand
+@REM echo Start program cat_brand.py...
+@REM python cat_brand.py
+@REM echo Exit code: %ERRORLEVEL%
+@REM if %ERRORLEVEL% neq 0 (
+@REM     echo Program cat_brand.py ends with errors. Rerun...
+@REM     goto cat_brand_review
+@REM )
+@REM echo Program cat_brand.py success
+@REM goto end
 
-@REM 重试抓取品牌子分类
-:cat_brand_review
-python cat_brand.py --review
-echo Exit code: %ERRORLEVEL%
-if %ERRORLEVEL% neq 0 (
-    echo Program cat_brand.py --review ends with errors. Rerun...
-    goto cat_brand_review
-)
-echo Program cat_brand.py success
-goto end
+@REM @REM 重试抓取品牌子分类
+@REM :cat_brand_review
+@REM python cat_brand.py --review
+@REM echo Exit code: %ERRORLEVEL%
+@REM if %ERRORLEVEL% neq 0 (
+@REM     echo Program cat_brand.py --review ends with errors. Rerun...
+@REM     goto cat_brand_review
+@REM )
+@REM echo Program cat_brand.py success
+@REM goto end
 
 @REM 抓取商品号
 :prod_id
