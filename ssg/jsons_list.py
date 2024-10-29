412d5dfb3523fdacf4d112ad66b9e1f0a5f1fc88
tony-ngok <tonyn1999@outlook.com>
2024-10-29 14:35:44 -0500

    bri

--- ssg/jsons_list.py
+++ ssg/jsons_list.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
         with open("ssg_products.txt", "r", encoding='utf-8') as f, open('json_tmp', "w", encoding='utf-8') as f_temp:
             f_temp.write("[\n")
             for line in f:
-                f_temp.write(f.read()[:-2])
+                f_temp.write(line)
             f_temp.write("\n]")
 
         os.remove("ssg_products.txt")
-        os.rename('json_tmp', f"ssg_products.json")
+        os.rename('json_tmp', "ssg_products.json")
     except Exception as e:
-        print("ERROR:", e)
+        print("ERROR:", str(e))
