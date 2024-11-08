d6b2b4597743ad537d00dd0b24e66ac027d12e4b
tony-ngok <tonyn1999@outlook.com>
2024-11-08 08:58:07 -0600

    eee

--- monotaro/monotaro/spiders/product_url.py
+++ monotaro/monotaro/spiders/product_url.py
@@ -4,7 +4,7 @@ import scrapy
 from scrapy.http import HtmlResponse
 
 
-# scrapy crawl monotaro_prod_url -O monotaro_prod_urls.json
+# scrapy crawl monotaro_prod_url
 class MonotaroProductUrl(scrapy.Spider):
     name = "monotaro_prod_url"
     allowed_domains = ["www.monotaro.com"]
@@ -13,7 +13,7 @@ class MonotaroProductUrl(scrapy.Spider):
 
     # https://docs.scrapy.org/en/2.11/topics/settings.html?highlight=retrymiddleware
     # https://docs.scrapy.org/en/2.11/topics/downloader-middleware.html#std-reqmeta-dont_redirect
-    handle_httpstatus_list = [301, 302]
+    handle_httpstatus_list = [301, 302, 404]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -46,14 +46,14 @@ class MonotaroProductUrl(scrapy.Spider):
             return catno_match[0]
 
     def start_requests(self):
-        for i, cu in enumerate(self.start_urls):
-            url = self.get_url(cu)
-            yield scrapy.Request(url, headers=self.get_headers(cu),
+        for i, cat_no in enumerate(self.start_urls):
+            url = self.get_url(cat_no)
+            yield scrapy.Request(url, headers=self.get_headers(cat_no),
                                  meta={ 'cookiejar': i },
                                  callback=self.parse,
-                                 cb_kwargs={ "i": i+1 })
+                                 cb_kwargs={ "i": i+1, "cat_no": cat_no })
 
-    def parse(self, response: HtmlResponse, i: int, p: int = 1):
+    def parse(self, response: HtmlResponse, i: int, cat_no: str, p: int = 1):
         print(f"{i:_}/{len(self.start_urls):_}".replace('_', '.'), response.url)
 
         prod_ax = response.css('div.SearchResultProductColumn')
@@ -64,16 +64,17 @@ class MonotaroProductUrl(scrapy.Spider):
             if img and (not (('mono_image_na' in img) or nomore)): # 没有画像和彻底断货的商品无法卖
                 href = a.css('div.SearchResultProductColumn__LeftColumn > a::attr(href)').get()
                 if href not in self.prod_nos:
-                    cat_no = self.get_no(href)
-                    self.prod_nos.add(cat_no)
-                    self.write_prod(cat_no)
+                    prod_no = self.get_no(href)
+                    self.prod_nos.add(prod_no)
+                    self.write_prod(prod_no)
 
         next_a = response.css('a.Button--PaginationNext').get()
         if next_a:
             next_url = self.get_url(cat_no, p+1)
             yield scrapy.Request(next_url, headers=self.get_headers(response.url),
-                                 meta={ "cookiejar": response.meta['cookiejar'] },
-                                 callback=self.parse, cb_kwargs={ "i": i, "p": p+1 })
+                                 meta={ 'cookiejar': response.meta['cookiejar'] },
+                                 callback=self.parse,
+                                 cb_kwargs={ "i": i, "cat_no": cat_no, "p": p+1 })
 
     def write_prod(self, prod: str):
         mod = 'a' if self.retry else 'w'
