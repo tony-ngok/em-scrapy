4b16ae9a7a24fab2c1e11e6883b025b133d33b9b
tony-ngok <tonyn1999@outlook.com>
2024-11-09 14:11:08 -0600

    monotaro：自动脚本

---
+++ monotaro/monotaro_auto.bat
@@ -0,0 +1,13 @@
+@REM 在Windows中，可运行taskschd.msc，创建一个新计画任务、定时（例如每日执行一次）、调用此脚本，即可实现定时自动抓取。
+
+@echo off
+cd /d "%~dp0"
+pip install -U scrapy
+pip install -U bs4
+pip install -U pymongo
+git pull
+
+del muji.log
+scrapy crawl monotaro_category
+scrapy crawl monotaro_prod_url
+scrapy crawl monotaro_product
