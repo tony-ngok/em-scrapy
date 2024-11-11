e24dc83b85e83052274a20d5588bcf6663ffad63
tony-ngok <tonyn1999@outlook.com>
2024-11-11 10:29:09 -0600

    429 - 0.1

--- trendyol/trendyol/spiders/prods_urls.py
+++ trendyol/trendyol/spiders/prods_urls.py
@@ -5,28 +5,30 @@ import scrapy
 from scrapy.http import HtmlResponse
 
 
-# scrapy crawl trendyol_prod_url -O trendyol_prods_urls.json
+# scrapy crawl trendyol_prod_url
 class TrendyolProdUrl(scrapy.Spider):
     name = 'trendyol_prod_url'
     allowed_domains = ['www.trendyol.com', 'apigw.trendyol.com']
     start_urls = []
 
+    HEADERS = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "fr-FR,fr;q=0.8,en-GB;q=0.5,en;q=0.3",
+        "Connection": "keep-alive",
+        "Priority": "u=0, i",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "fr-FR,fr;q=0.8,en-GB;q=0.5,en;q=0.3",
-            "Connection": "keep-alive",
-            "Priority": "u=0, i",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
-        }
+        
     
         with open('trendyol_categories.json', 'r', encoding='utf-8') as f_in:
             self.start_urls = [cat['cat_url'] for cat in json.load(f_in)]
